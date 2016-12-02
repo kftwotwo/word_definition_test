7ac97c5a58fddab73168adb526e924d4446d4442
Kevin Finley <kftwotwo@gmail.com>
2016-12-02 08:10:11 -0800

    it will create an array and clear it

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,15 @@
 class Word
   attr_reader(:name)
 
+  @@words = []
   def initialize(attributes)
     @name = attributes[:name]
   end
+
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+  # def add
+  #   @@words.push(self)
+  # end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -9,4 +9,19 @@ describe(Word) do
     end
   end
 
+  describe('.clear') do
+    it "will clear out the array" do
+      expect(Word.clear()).to(eq([]))
+    end
+  end
+
+  # describe('#add') do
+  #   it "will add word to an array" do
+  #     word = Word.new(:name => 'Computer')
+  #     word.add()
+  #     expect(word.add()).to(eq([word]))
+  #   end
+  # end
+
+
 end
